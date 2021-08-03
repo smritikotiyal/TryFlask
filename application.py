0b93ab455df52242f4e474638f7bb53a755d6195
smritikotiyal <smritikotiyal@gmail.com>
2021-08-03 13:01:26 +0100

    Trying application name

--- application.py
+++ application.py
@@ -1,15 +1,13 @@
 from flask import Flask, jsonify, request
-app = Flask(__name__)
+application = Flask(__name__)
 
-application = app
-
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     return "Hello World!"
 
-@app.route('/multi/<int:num>', methods=['GET'])
+@application.route('/multi/<int:num>', methods=['GET'])
 def get_multiply10(num):
     return jsonify({"result" : num*10})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
