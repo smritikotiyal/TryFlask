bcd0eaa7d0add3f3829292ae14f9d0107a304df2
smritikotiyal <smritikotiyal@gmail.com>
2021-08-03 12:37:28 +0100

    Added debug line

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask 
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 application = app
 
-@app.route('/')
-def hello():
-    return 'Hello!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Hello World!"
+
+@app.route('/multi/<int:num>', methods=['GET'])
+def get_multiply10(num):
+    return jsonify({"result" : num*10})
+
+if __name__ == '__main__':
+    app.run(debug=True)
