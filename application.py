72e0d73cfa1c69300cc8496907855ff2124858e9
smritikotiyal <smritikotiyal@gmail.com>
2021-08-13 13:51:01 +0100

    NB Added

--- application.py
+++ application.py
@@ -1,13 +1,84 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+import numpy as np
+import pandas as pd
+import json
+
+# For model dumping
+import joblib
+
+
 application = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
+@application.route('/')
 def index():
-    return "Hello World!"
+    return render_template('register.html')
 
-@application.route('/multi/<int:num>', methods=['GET'])
-def get_multiply10(num):
-    return jsonify({"result" : num*10})
+# 2 decorators same function
+@application.route('/', methods=['GET', 'POST'])
+@application.route('/<id>', methods=['GET', 'POST'])
+def socCode(id='socCode'):
+    
+    print('Here')
+    print(socCode)
+    if request.method == "POST":
+        JD = request.form["jd"]
+        print(JD)
+
+    prediction = {}
+    df_description = pd.read_csv('./static/data/AYJ_Aug_Description.csv')
+    classes = list(set(df_description['SOC']))
+    df_prob = pd.DataFrame({"SOC": classes})
+    df_prob.sort_values(by='SOC', ascending=True, inplace=True)
+    
+    def modelPredict(data):
+        
+        result_rf = {}
+        result_nb = {}
+        df_data = pd.DataFrame({"JD": [data]})
+
+        # load, no need to initialize the loaded_rf
+        print('load models')
+        # loaded_model = joblib.load("./static/models/random_forest.joblib")
+        # loaded_model_nb = joblib.load("./static/models/naive_baeyes.joblib")
+        loaded_model = joblib.load("./static/models/naive_baeyes.joblib")
+
+        pred = loaded_model.predict(df_data['JD'])
+        y_proba = loaded_model.predict_proba(df_data['JD'])
+
+        df_prob['RF_prob'] = y_proba[0]
+        df_prob.sort_values(by='RF_prob', ascending=False, inplace=True)
+
+        most_prob_soc = [df_prob['SOC'].iloc[0], df_prob['SOC'].iloc[1], df_prob['SOC'].iloc[2]]
+        print(most_prob_soc)
+
+        # df.drop(['column_1', 'Column_2'], axis = 1, inplace = True)
+        df_prob.drop(['RF_prob'], axis=1, inplace=True)
+        df_prob.sort_values(by='SOC', ascending=True, inplace=True)
+
+
+        for soc in most_prob_soc:
+            for i in range(0, len(df_description)):
+                if (soc == df_description['SOC'].iloc[i]):
+                    desc = df_description['Description'].iloc[i]
+                    prediction[str(soc)] = str(soc) + ', ' + desc
+                    break
+            print(prediction)
+        
+        return prediction
+        # return result_rf, result_nb
+
+    def predictSOC(input_data):
+        prediction = {}
+        # Prediction to find SOC of new Data
+        predict_soc = modelPredict(input_data)
+        return predict_soc
+    
+    details = predictSOC(JD)
+    print(details)
+    json_object = json.dumps(details, indent = 4)
+    print(json_object)
+
+    return render_template('register.html', data=[json_object])
 
 if __name__ == '__main__':
     application.run(debug=True)
